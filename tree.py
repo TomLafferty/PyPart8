07e89a64053fe9ca17c0b9063b716f89016efa08
Thomas Lafferty <tsl5046@gmail.com>
2021-11-21 16:58:21 -0500

    updated exersice 3

--- tree.py
+++ tree.py
@@ -1,3 +1,19 @@
+import os
+
+def print_file_paths(file_path):
+    '''Given a file path, write to a file all of the contents of the directory and child directories below it'''
+
+    # new_file = open(file_path, 'w')
+    # for line in new_file:
+    #     new_file.write(line)
+    contents = os.listdir(file_path)
+    for item in contents:
+        print(os.path.relpath(item))
+
+
+example = ('/Users/laffertythomas/dev/PyParts/PyPart8')
+print_file_paths(example)
+
 
 
 # ./file1.py
